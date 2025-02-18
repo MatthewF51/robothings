2b89087d4dc20605b0369957e1d833ac5d60bd82
MatthewF51 <114248397+MatthewF51@users.noreply.github.com>
2025-02-18 13:06:53 +0000

    Update movement.py

--- Modules/movement.py
+++ Modules/movement.py
@@ -1,65 +1,66 @@
 from utils import send_command
+from geometry_msgs.msg import Twist  # Importing correct message type
 
 COMMANDS = {
     "Move Forward": {
         "function": lambda speed, distance: send_command(
             "/cmd_vel",
-            {
-                "linear": {"x": float(speed), "y": 0.0, "z": 0.0},
-                "angular": {"x": 0.0, "y": 0.0, "z": 0.0}
-            },
-            duration=(float(distance) / float(speed)) if float(speed) != 0 else 0
+            Twist,
+            linear_x=float(speed),
+            angular_z=0.0,
+            duration=float(distance) / float(speed)
         ),
         "inputs": {"Speed": "speed", "Distance": "distance"},
-        "topic": "/cmd_vel"
+        "topic": "/cmd_vel",
+        "msg_type": Twist  # Explicitly defining message type
     },
     "Move Backward": {
         "function": lambda speed, distance: send_command(
             "/cmd_vel",
-            {
-                "linear": {"x": -float(speed), "y": 0.0, "z": 0.0},
-                "angular": {"x": 0.0, "y": 0.0, "z": 0.0}
-            },
-            duration=(float(distance) / float(speed)) if float(speed) != 0 else 0
+            Twist,
+            linear_x=-float(speed),
+            angular_z=0.0,
+            duration=float(distance) / float(speed)
         ),
         "inputs": {"Speed": "speed", "Distance": "distance"},
-        "topic": "/cmd_vel"
+        "topic": "/cmd_vel",
+        "msg_type": Twist
     },
     "Rotate Left": {
         "function": lambda speed, angle: send_command(
             "/cmd_vel",
-            {
-                "linear": {"x": 0.0, "y": 0.0, "z": 0.0},
-                "angular": {"x": 0.0, "y": 0.0, "z": float(speed)}
-            },
-            duration=(float(angle) / float(speed)) if float(speed) != 0 else 0
+            Twist,
+            linear_x=0.0,
+            angular_z=float(speed),
+            duration=float(angle) / float(speed)
         ),
         "inputs": {"Speed": "speed", "Angle": "angle"},
-        "topic": "/cmd_vel"
+        "topic": "/cmd_vel",
+        "msg_type": Twist
     },
     "Rotate Right": {
         "function": lambda speed, angle: send_command(
             "/cmd_vel",
-            {
-                "linear": {"x": 0.0, "y": 0.0, "z": 0.0},
-                "angular": {"x": 0.0, "y": 0.0, "z": -float(speed)}
-            },
-            duration=(float(angle) / float(speed)) if float(speed) != 0 else 0
+            Twist,
+            linear_x=0.0,
+            angular_z=-float(speed),
+            duration=float(angle) / float(speed)
         ),
         "inputs": {"Speed": "speed", "Angle": "angle"},
-        "topic": "/cmd_vel"
+        "topic": "/cmd_vel",
+        "msg_type": Twist
     },
     "Stop": {
         "function": lambda: send_command(
             "/cmd_vel",
-            {
-                "linear": {"x": 0.0, "y": 0.0, "z": 0.0},
-                "angular": {"x": 0.0, "y": 0.0, "z": 0.0}
-            }
+            Twist,
+            linear_x=0.0,
+            angular_z=0.0
         ),
         "inputs": {},
-        "topic": "/cmd_vel"
+        "topic": "/cmd_vel",
+        "msg_type": Twist
     }
 }
 
-colour = "#FF5733"
+colour = "#FF5733"  # Orange color for UI
